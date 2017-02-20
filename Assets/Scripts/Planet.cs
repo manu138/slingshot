3b2153de4fe7b5409faa5c454b86fa662dc15bc2
Héctor C. Alzate <halzate93@gmail.com>
2017-02-19 23:26:51 -0500

    Initial setup

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class Planet : MonoBehaviour 
+{
+	[SerializeField]
+	private float maxTorque;
+	private new Rigidbody2D rigidbody;
+
+	private void Awake ()
+	{
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	private void Start () 
+	{
+		AddRandomTorque ();	
+	}
+	
+	private void AddRandomTorque ()
+	{
+		float torque = Random.Range (-maxTorque, maxTorque);
+		rigidbody.AddTorque (torque);
+	}
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class Ship : MonoBehaviour 
+{
+	private new Rigidbody2D rigidbody;
+
+	private void Awake () 
+	{
+		rigidbody = GetComponent<Rigidbody2D> ();	
+	}
+	
+	void Update () 
+	{
+
+	}
+
+	private void LookTowards (Vector2 direction)
+	{
+		
+		transform.localRotation = Quaternion.LookRotation (Vector3.forward, direction);
+	}
+}
