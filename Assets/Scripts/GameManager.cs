a415ad2049fdf51a60f86fb768714207e8c45d30
Héctor C. Alzate <halzate93@gmail.com>
2017-02-19 23:32:49 -0500

    Created score manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+	public static GameManager Instance
+	{
+		get; private set;
+	}
+
+	[SerializeField]
+	private Text scoreLabel;
+	private int points;
+
+	private void Awake ()
+	{
+		if (Instance != null)
+			Destroy (gameObject);
+		else
+			Instance = this;
+	}
+
+	public void IncreaseScore (int points)
+	{
+		this.points += points;
+		scoreLabel.text = string.Format ("Score: {0}", points);
+	}
+
+	public void WinGame ()
+	{
+		scoreLabel.text = string.Format ("Level completed!");
+	}
+}
