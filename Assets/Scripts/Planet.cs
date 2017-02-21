582df79aa722d2fcb21bdf023f3a0e4a87ff0f1c
manu138 <manu138@hotmail.es>
2017-02-21 11:33:12 -0500

    Primer commit

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -6,8 +6,11 @@ public class Planet : MonoBehaviour
 	[SerializeField]
 	private float maxTorque;
 	private new Rigidbody2D rigidbody;
+    [SerializeField]
+    private int speed;
 
-	private void Awake ()
+
+    private void Awake ()
 	{
 		rigidbody = GetComponent<Rigidbody2D> ();
 	}
@@ -22,4 +25,25 @@ public class Planet : MonoBehaviour
 		float torque = Random.Range (-maxTorque, maxTorque);
 		rigidbody.AddTorque (torque);
 	}
+    void Update()
+    {
+       
+      
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosScreen = Input.mousePosition;
+            Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(mousePosScreen);
+
+
+
+            Vector3 posB = new Vector3(mousePosWorld.x - transform.position.x, mousePosWorld.y - transform.position.y, mousePosWorld.z - transform.position.z);
+            Mathf.Clamp(posB.magnitude, 0, 10);
+
+
+            rigidbody.AddForce(posB.normalized * speed, ForceMode2D.Impulse);
+
+        }
+    }
 }
